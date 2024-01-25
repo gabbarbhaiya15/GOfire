758b217c1cf4f261f63a4aa7972a39d5c4d9f375
gabbarbhaiya15 <143590849+gabbarbhaiya15@users.noreply.github.com>
2024-01-26 00:47:59 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port =  process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors({credentials: true,
-  origin: ['https://gofirebackend.onrender.com']
+  origin: ['https://gofirfrontend.onrender.com']
 
 }));
 app.use(cookieParser());
