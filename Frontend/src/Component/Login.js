cef43a1df63d8749f1e15e85bc77ab03ba0fce13
gabbarbhaiya15 <143590849+gabbarbhaiya15@users.noreply.github.com>
2024-01-26 04:40:40 +0530

    Update Login.js

--- Frontend/src/Component/Login.js
+++ Frontend/src/Component/Login.js
@@ -5,55 +5,39 @@ import { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from "react-router-dom";
 
-
-
- export default function Login(){
+export default function Login() {
   const [LEmail, setLEmail] = useState("");
-  const  [Lpassword, setLpassword] = useState("");
-  const navigate = useNavigate() ;
+  const [Lpassword, setLpassword] = useState("");
+  const navigate = useNavigate();
 
   const Checkdatas = async (e) => {
+    e.preventDefault();
     console.log("checking");
-     e.preventDefault();
-   await axios.post('https://gofirebackend.onrender.com/login',{LEmail,Lpassword},{withCredentials:true})
-   .then((users) => { 
-   console.log("login in")
-   alert("welcome 🎊🎊🎉🎇 ")
-   navigate('/')
-
-  
-   
-  
-  
-   
-  
-  })
-   .catch((error) => {console.log("error")
-   alert(" User Not Found 😒😒😒 ")
-   window.location.reload();
-  });
-    
+    await axios.post('https://gofirebackend.onrender.com/login', { LEmail, Lpassword }, { withCredentials: true })
+      .then((users) => {
+        console.log("login in")
+        alert("welcome 🎊🎊🎉🎇 ")
+        navigate('/');
+      })
+      .catch((error) => {
+        console.log("error");
+        alert("User Not Found 😒😒😒 ");
+        window.location.reload();
+      });
   };
 
-
-
-    return(
+  return (
     <form action="https://gofirebackend.onrender.com/login" onSubmit={Checkdatas} method="post">
-    <div class="card">
-    <div class="circle"></div>
-    <div class="circle"></div>
-    <div class="card-inner">
-    <p class="title">Login Form</p>
-            <input placeholder="Email"   class="username input" type="email" value={LEmail}  onChange = {(e)=>  setLEmail(e.target.value) } />
-            <input placeholder="Password"    class="password input" type="password"  value={Lpassword}  onChange = {(e)=>  setLpassword(e.target.value) } />
-   
-            <button type="submit" class=" login" >Login</button>
-
-    </div>
-
-</div>
-
-    
+      <div class="card">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="card-inner">
+          <p class="title">Login Form</p>
+          <input placeholder="Email" class="username input" type="email" value={LEmail} onChange={(e) => setLEmail(e.target.value)} />
+          <input placeholder="Password" class="password input" type="password" value={Lpassword} onChange={(e) => setLpassword(e.target.value)} />
+          <button type="submit" className="login">Login</button>
+        </div>
+      </div>
     </form>
-    )
- }
+  );
+}
