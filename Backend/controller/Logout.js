b56c50968d46945c6e0d602b3ca73032038b391c
gabbarbhaiya15 <143590849+gabbarbhaiya15@users.noreply.github.com>
2024-02-01 21:40:31 +0530

    Update Logout.js

--- Backend/controller/Logout.js
+++ Backend/controller/Logout.js
@@ -4,21 +4,22 @@ const cookieParser = require('cookie-parser');
 const JWTModel = require('../Modules/UserModel');
 const jwt = require('jsonwebtoken');
 
-    
-
-router.get('/', async (req,res)=>{
-const {token} = req.cookies;
 
 
-try{
-res.clearCookie('token');
-    console.log("logging out here ")
 
-return res.status(200).json({message: 'Logout successful'})
-}
-catch(err){
-return res.status(400).send({message: err.message});
-}
+router.get('/', (req, res) => {
+  const { token } = req.cookies;
 
-})
+  try {
+    // Set the 'Set-Cookie' header with an expired date and SameSite=None
+    res.setHeader('Set-Cookie', `token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=None; Secure;`);
+    console.log("Manually deleting cookie");
+    return res.status(200).json({ message: 'Logout successful' });
+  } catch (err) {
+    return res.status(400).send({ message: err.message });
+  }
+});
+
+module.exports = router;
+
 module.exports= router;
